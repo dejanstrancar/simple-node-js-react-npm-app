43e8a342efeb4ee46c4880639bbf8bab4beae845
Dejan Strancar <dejan@strancar.info>
2025-03-01 09:17:46 +0100

    Updating deploy

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-SERVER_ADDRESS=$(echo $SERVER_ADDRESS)
-SERVER_PASSWORD=$(echo $SERVER_PASSWORD)
 set -x
-caprover deploy -h $SERVER_ADDRESS -p "$SERVER_PASSWORD" -b master -a simple-node-js-react-npm-app
+caprover deploy -b master -a simple-node-js-react-npm-app
 set +x
