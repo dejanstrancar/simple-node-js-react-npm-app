ce2783edc2454b4c9bdec1272158246b36ffb820
Dejan Strancar <dejan@strancar.info>
2025-03-01 09:14:38 +0100

    moving around

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
-
-set -x
 SERVER_ADDRESS=$(echo $SERVER_ADDRESS)
 SERVER_PASSWORD=$(echo $SERVER_PASSWORD)
+set -x
 caprover deploy -h $SERVER_ADDRESS -p "$SERVER_PASSWORD" -b master -a simple-node-js-react-npm-app
 set +x
