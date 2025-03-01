af604b06b405a3cfbe72baf635f80724fd7f989c
Dejan Strancar <dejan@strancar.info>
2025-03-01 09:12:20 +0100

    Adding caprover config

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-caprover deploy --default
+SERVER_ADDRESS=$(echo $SERVER_ADDRESS)
+SERVER_PASSWORD=$(echo $SERVER_PASSWORD)
+caprover deploy -h $SERVER_ADDRESS -p "$SERVER_PASSWORD" -b master -a simple-node-js-react-npm-app
 set +x
